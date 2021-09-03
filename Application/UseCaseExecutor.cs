a231b5424ecc364dc7f088ba5ab3aa926ecb1e8a
Milica Zivanic <milicazivanic97@gmail.com>
2021-09-03 14:18:19 +0200

    Set up architecture

---
+++ Application/UseCaseExecutor.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application
+{
+    public class UseCaseExecutor
+    {
+        private readonly IApplicationActor actor;
+
+        public UseCaseExecutor(IApplicationActor actor)
+        {
+            this.actor = actor;
+        }
+
+        public void ExecuteCommand<TRequest>(ICommand<TRequest> command, TRequest request)
+        {
+
+        }
+    }
+}
---
+++ DataAccess/EcomShopContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace EfDataAccess
+{
+    public class EcomShopContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer();
+        }
+    }
+}
